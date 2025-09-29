c3770d0e7319511054bbb45ea3b1082d39b62fe7
William Morales <wsmbuis5224@gmail.com>
2025-09-28 19:18:24 -0500

    adding checkbox for whole word and accents

--- creoledict/views.py
+++ creoledict/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 from .models import Entry
+from django.db.models import Q
 
 def search_dictionary(request):
     query = request.GET.get('q', '')
-    field = request.GET.get('field', 'headword')  # default search in headword
+    field = request.GET.get('field', 'headword')
+    whole_word = 'whole_word' in request.GET
+    match_accents = 'match_accents' in request.GET
+
     results = []
 
     if query:
-        if field == 'headword':
-            results = Entry.objects.filter(headword__icontains=query)
-        elif field == 'content':
-            results = Entry.objects.filter(content__icontains=query)
-    
+        search_query = query
+
+        # If not matching accents, normalize text to remove accents
+        if not match_accents:
+            import unicodedata
+            def strip_accents(s):
+                return ''.join(c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn')
+            search_query = strip_accents(search_query)
+            # Note: In real use, you might also strip accents from model fields during filtering
+
+        # Construct lookup
+        if whole_word:
+            lookup = f'{field}__regex'
+            regex = fr'\b{search_query}\b'  # \b = word boundary
+            results = Entry.objects.filter(**{lookup: regex})
+        else:
+            lookup = f'{field}__icontains'
+            results = Entry.objects.filter(**{lookup: search_query})
+
     context = {
         'query': query,
         'field': field,
-        'results': results,
+        'whole_word': whole_word,
+        'match_accents': match_accents,
+        'results': results
     }
     return render(request, 'search.html', context)
